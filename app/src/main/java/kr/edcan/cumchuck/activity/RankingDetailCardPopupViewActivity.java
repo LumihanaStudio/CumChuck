47de88a32641d1325d928aefe62a0d547ebcc677
Junseok Oh <kotohana5706@edcan.kr>
2016-06-09 10:13:47 +0900

    Implement RankingDetailCardPopupViewActivity

---
+++ app/src/main/java/kr/edcan/cumchuck/activity/RankingDetailCardPopupViewActivity.java
@@ -0,0 +1,13 @@
+package kr.edcan.cumchuck.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RankingDetailCardPopupViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking_detail_card_popup_view);
+    }
+}
