6cd96fdc8102c14cbc13083293066e50b292fb3d
Junseok Oh <kotohana5706@edcan.kr>
2016-06-11 09:56:57 +0900

    Implement RaidGenerateActivitY

---
+++ app/src/main/java/kr/edcan/cumchuck/RaidGenerateActivity.java
@@ -0,0 +1,13 @@
+package kr.edcan.cumchuck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RaidGenerateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raid_generate);
+    }
+}
