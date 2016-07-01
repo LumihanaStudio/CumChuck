3c2edaaae8340d5e4ff9a203614ff1a13163591f
Junseok Oh <kotohana5706@edcan.kr>
2016-07-02 04:22:29 +0900

    Update MyReviewActivity Default

--- app/src/main/java/kr/edcan/cumchuck/activity/MyReviewActivity.java
+++ app/src/main/java/kr/edcan/cumchuck/activity/MyReviewActivity.java
@@ -1,15 +1,42 @@
 package kr.edcan.cumchuck.activity;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import kr.edcan.cumchuck.R;
 
 public class MyReviewActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_review);
+        setAppbarLayout();
+        setDefault();
+    }
+
+    private void setAppbarLayout() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("내 리뷰 관리");
+        getSupportActionBar().setElevation(5);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Drawable drawable = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        drawable.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
+    }
+
+    private void setDefault() {
+        recyclerView = (RecyclerView) findViewById(R.id.myreview_recyclerview);
+
     }
 }
