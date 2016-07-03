eab3c9741b9c79034020eabc49c8b83cafc4a5d1
Junseok Oh <kotohana5706@edcan.kr>
2016-07-04 03:47:31 +0900

    MyReviewDetailView: Layout Prototype Complete

--- app/src/main/java/kr/edcan/cumchuck/activity/MyReviewDetailViewActivity.java
+++ app/src/main/java/kr/edcan/cumchuck/activity/MyReviewDetailViewActivity.java
@@ -1,15 +1,57 @@
 package kr.edcan.cumchuck.activity;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import kr.edcan.cumchuck.R;
+import kr.edcan.cumchuck.model.MyReviewData;
 
 public class MyReviewDetailViewActivity extends AppCompatActivity {
+
+    String reviewId;
+    MyReviewData data;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_review_detail_view);
+        setDefault();
+        setAppbarLayout();
+
+    }
+
+    private void setAppbarLayout() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("내가 작성한 리뷰");
+        getSupportActionBar().setElevation(5);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Drawable drawable = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        drawable.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
+    }
+
+    private void setDefault() {
+        reviewId = getIntent().getStringExtra("reviewId");
+        if (!reviewId.trim().equals("")) {
+//            data = ""
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
