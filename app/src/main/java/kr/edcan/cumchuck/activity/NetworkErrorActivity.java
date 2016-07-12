bb55d290d19820b3bcf88dd2b60aa7befadced19
Junseok Oh <kotohana5706@edcan.kr>
2016-07-12 11:39:27 +0900

    Implement NetworkErrorActivity

---
+++ app/src/main/java/kr/edcan/cumchuck/activity/NetworkErrorActivity.java
@@ -0,0 +1,13 @@
+package kr.edcan.cumchuck.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_error);
+    }
+}
